697e0cdf1df1b3a440474ddafa1e8ed5556ef90b
Luqman Fadlli <luqman.fadlli@gmail.com>
2026-09-14 15:46:37 +0700

    chore(home): drop unused BoxWithConstraints import from HomeScreen.kt

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/home/HomeScreen.kt
@@ -2,7 +2,6 @@ package com.nuvio.app.features.home
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListScope
